e010362a107943238ac180270b317d6c3d2dce06
Subodh Shakya <subodhshakya@live.com>
2018-11-04 21:40:30 -0600

    Is unique question added.

--- ProblemSolving/ArraysAndStrings/Solutions/IsUnique.cs
+++ ProblemSolving/ArraysAndStrings/Solutions/IsUnique.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace ArraysAndStrings.Solutions
 {
+    /* ***************************************************************************************
+     * Is Unique: Implement an algorithm to determine if a string has all unique characters. 
+     * What if you cannot use additional data structures     
+     * ***************************************************************************************/
     public class IsUnique
     {
 
